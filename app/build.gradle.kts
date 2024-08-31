87d22ba5e2de04a6f0d32b62820d9f435b19f861
Uchyy <uchenwaobiala@gmail.com>
2024-08-31 22:51:41 +0100

    update .gitignore

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("com.android.application")
+    id ("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.meshach.tictactoe"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.meshach.tictactoe"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        create("customDebugType") {
+            isDebuggable = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test:rules:1.6.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.6.1")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.firebase:firebase-analytics")
+}
---
+++ app/src/main/java/com/meshach/tictactoe/StartUp.java
@@ -0,0 +1,148 @@
+package com.meshach.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.meshach.tictactoe.Classes.GameViewModel;
+import com.meshach.tictactoe.GamePlay.GameManager;
+
+import java.util.HashMap;
+
+public class StartUp extends AppCompatActivity implements View.OnClickListener{
+
+    //HashMap<String, String> userDetails = new HashMap<String, String>();
+    private String userPlayer; private int board; private boolean vsCPU;
+    private Button buttonX;
+    private Button buttonO;
+    private Button buttonVsCPU;
+    private Button goBack, by3Button, by5Button;
+    private Button buttonVsPlayer;
+    private ConstraintLayout linearLayoutBoard;
+    private MyAnimations anim;
+    private Boolean resetGame, newRound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        Intent intent = getIntent();
+        resetGame = intent.getBooleanExtra("resetGame", false);
+        Log.d("START-UP RESET VALUE: ", Boolean.toString(resetGame));
+        if (resetGame) {
+           /* GameViewModel viewModel = new ViewModelProvider(this).get(GameViewModel.class);
+            viewModel.resetGame();*/
+
+            GameManager manager = new GameManager(getApplicationContext(), this);
+            manager.reset();
+        }
+
+        GameViewModel gameViewModel = new ViewModelProvider(this).get(GameViewModel.class);
+        gameViewModel.resetGame();
+
+        newRound = intent.getBooleanExtra("newRound", false);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_up);
+        anim = new MyAnimations();
+        userPlayer = "O";
+
+        buttonO = findViewById(R.id.buttonO);
+        buttonO.setOnClickListener(this);
+        buttonX = findViewById(R.id.buttonX);
+        buttonX.setOnClickListener(this);
+        buttonVsCPU = findViewById(R.id.vsCPu);
+        buttonVsCPU.setOnClickListener(this);
+
+        linearLayoutBoard = findViewById(R.id.viewChooseBoard);
+
+        buttonVsPlayer = findViewById(R.id.vsPlayer);
+        buttonVsPlayer.setOnClickListener(this);
+        goBack = linearLayoutBoard.findViewById(R.id.backBtn);
+        goBack.setOnClickListener(this);
+        by3Button =  linearLayoutBoard.findViewById(R.id.by3Button);
+        by3Button.setOnClickListener(this);
+        by5Button =  linearLayoutBoard.findViewById(R.id.by5Button);
+        by5Button.setOnClickListener(this);
+
+        linearLayoutBoard.setVisibility(View.INVISIBLE);
+
+    }
+
+    private void setPlayer(View btn1, View btn2) {
+
+        if (btn1 instanceof Button && btn2 instanceof Button) {
+            btn1.setBackgroundResource(R.drawable.thumb);
+            ((Button) btn1).setTextColor(ContextCompat.getColor(this, R.color.darkblue));
+
+            btn2.setBackgroundResource(R.drawable.track);
+            ((Button) btn2).setTextColor(ContextCompat.getColor(this, R.color.gray));
+
+            userPlayer = ((Button) btn1).getText().toString() ;
+
+            Toast.makeText(this, "You are Player "+userPlayer+"!", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = new Intent(StartUp.this, MainActivity.class);
+        int id = view.getId();
+
+        if (id == R.id.buttonO) {
+            setPlayer(view, buttonX);
+            userPlayer = "O";
+        }
+        else if (id == R.id.buttonX) {
+            setPlayer(view, buttonO);
+            userPlayer = "X";
+        }
+        else if (id == R.id.vsCPu) {
+            vsCPU = true;
+            anim.fadeIn(linearLayoutBoard);
+        }
+        else if (id == R.id.vsPlayer) {
+            vsCPU = false;
+            anim.fadeIn(linearLayoutBoard);
+        }
+        else if (id == R.id.backBtn) {
+            anim.fadeOut(linearLayoutBoard);
+        }
+        else if (id == R.id.by3Button ||  id == R.id.by5Button) {
+
+            if (id == R.id.by5Button) {
+                board = 5;
+                //startActivity(intent);
+            } else if (id == R.id.by3Button) {
+                board = 3;
+                //startActivity(intent);
+            }
+
+            intent.putExtra("player1", userPlayer);
+            intent.putExtra("board", board);
+            intent.putExtra("vsCPU", vsCPU);
+            intent.putExtra("resetGame", resetGame);
+            intent.putExtra("newRound", newRound);
+            anim.fadeOut(linearLayoutBoard);
+            //anim.fadeOutActivity(this);
+            new Handler().postDelayed(() -> startActivity(intent), 1000);
+        }
+
+    }
+
+}
